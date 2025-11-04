26f8e966621f83d63c4565cf1066ab3517d31e62
diegotosato <tosato1998@gmail.com>
2025-11-04 16:57:59 +0100

    Fix CSS variable names and update navigation class attributes for consistency

--- src/App.jsx
+++ src/App.jsx
@@ -13,17 +13,17 @@ function App() {
             <img src={logo} alt="DC_logo" />
           </div>
 
-          <nav class="nav justify-content-center">
-            <a class="nav-link" href="#">characters</a>
-            <a class="nav-link" href="#">comics</a>
-            <a class="nav-link" href="#">movies</a>
-            <a class="nav-link" href="#">tv</a>
-            <a class="nav-link" href="#">games</a>
-            <a class="nav-link" href="#">collectibles</a>
-            <a class="nav-link" href="#">videos</a>
-            <a class="nav-link" href="#">fans</a>
-            <a class="nav-link" href="#">news</a>
-            <a class="nav-link" href="#">shop</a>
+          <nav className="nav justify-content-center">
+            <a className="nav-link" href="#">characters</a>
+            <a className="nav-link" href="#">comics</a>
+            <a className="nav-link" href="#">movies</a>
+            <a className="nav-link" href="#">tv</a>
+            <a className="nav-link" href="#">games</a>
+            <a className="nav-link" href="#">collectibles</a>
+            <a className="nav-link" href="#">videos</a>
+            <a className="nav-link" href="#">fans</a>
+            <a className="nav-link" href="#">news</a>
+            <a className="nav-link" href="#">shop</a>
           </nav>
 
         </div>
