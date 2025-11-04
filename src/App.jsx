b9775cebe31e432747b2dc2be454bfbdbee6e8fa
diegotosato <tosato1998@gmail.com>
2025-11-04 16:53:05 +0100

    Add header with navigation links and update styles for layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,35 @@ function App() {
 
   return (
     <>
+      <header>
+
+        <div className="container">
+
+          <div className="logo">
+            <img src={logo} alt="DC_logo" />
+          </div>
+
+          <nav class="nav justify-content-center">
+            <a class="nav-link" href="#">characters</a>
+            <a class="nav-link" href="#">comics</a>
+            <a class="nav-link" href="#">movies</a>
+            <a class="nav-link" href="#">tv</a>
+            <a class="nav-link" href="#">games</a>
+            <a class="nav-link" href="#">collectibles</a>
+            <a class="nav-link" href="#">videos</a>
+            <a class="nav-link" href="#">fans</a>
+            <a class="nav-link" href="#">news</a>
+            <a class="nav-link" href="#">shop</a>
+          </nav>
+
+        </div>
+
+      </header>
+
+
+
+      <main></main>
+      <footer></footer>
 
 
     </>
