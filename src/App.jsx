1e2f775a479066450ec66feef566bb2d53da55b2
diegotosato <tosato1998@gmail.com>
2025-11-04 16:59:40 +0100

    Refactor header component into separate file and update App component to use it

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,11 @@
 import './App.css'
-import logo from './assets/img/dc-logo.png'
+import Header from './contents/Header'
 
 function App() {
 
   return (
     <>
-      <header>
-
-        <div className="container">
-
-          <div className="logo">
-            <img src={logo} alt="DC_logo" />
-          </div>
-
-          <nav className="nav justify-content-center">
-            <a className="nav-link" href="#">characters</a>
-            <a className="nav-link" href="#">comics</a>
-            <a className="nav-link" href="#">movies</a>
-            <a className="nav-link" href="#">tv</a>
-            <a className="nav-link" href="#">games</a>
-            <a className="nav-link" href="#">collectibles</a>
-            <a className="nav-link" href="#">videos</a>
-            <a className="nav-link" href="#">fans</a>
-            <a className="nav-link" href="#">news</a>
-            <a className="nav-link" href="#">shop</a>
-          </nav>
-
-        </div>
-
-      </header>
+      <Header />
 
 
 
