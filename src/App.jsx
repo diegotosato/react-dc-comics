ed322b8eafaf41ae58fde25f8c8cdfb65a0fd294
diegotosato <tosato1998@gmail.com>
2025-11-04 17:14:58 +0100

    Add main content and banner sections with icons to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Header from './contents/Header'
+import Icon from './assets/img/buy-comics-digital-comics.png'
 
 function App() {
 
@@ -9,7 +10,62 @@ function App() {
 
 
 
-      <main></main>
+      <main>
+        <div className="container">
+          <section className="content">
+            <h3>-- Content goes here --</h3>
+          </section>
+
+        </div>
+      </main>
+
+      <section className="banner">
+
+        <div className="container">
+
+          <ul>
+
+            <li>
+              <div className="icon">
+                <img src={Icon} alt="icon_image" />
+              </div>
+              <a href="#">digital comics</a>
+            </li>
+
+            <li>
+              <div className="icon">
+                <img src={Icon} alt="icon_image" />
+              </div>
+              <a href="#">digital comics</a>
+            </li>
+
+            <li>
+              <div className="icon">
+                <img src={Icon} alt="icon_image" />
+              </div>
+              <a href="#">digital comics</a>
+            </li>
+
+            <li>
+              <div className="icon">
+                <img src={Icon} alt="icon_image" />
+              </div>
+              <a href="#">digital comics</a>
+            </li>
+
+            <li>
+              <div className="icon">
+                <img src={Icon} alt="icon_image" />
+              </div>
+              <a href="#">digital comics</a>
+            </li>
+
+          </ul>
+
+        </div>
+
+      </section>
+
       <footer></footer>
 
 
